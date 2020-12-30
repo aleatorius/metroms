c3add82ccd3038aa7416e344ebce36e816b5db09
Dmitry Shcherbin <mitya@login-2.betzy.sigma2.no>
2020-12-30 13:42:39 +0100

    trying gompi

---
+++ apps/compile_everything_gompi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source myenv_gfram.bash
+source modules_gompi.sh
+
+./build_mct.sh
+./build_cice_gbetzy.sh SA4 12 8
+#./build_roms_betzy.sh SA4 -j 4 
+
---
+++ apps/modules_gompi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fram
+
+
+
+
+
+module purge
+module load StdEnv
+#module load netCDF-Fortran/4.5.2-gompi-2020a
+module load netCDF-Fortran/4.4.5-gompi-2019a
+export I_MPI_F90=gfortran
+
+
+#intel 2018a
+
+#module purge
+#module load StdEnv
+#module load intel/2018a
+#module load netCDF-Fortran/4.4.4-intel-2018a-HDF5-1.8.19
+#export I_MPI_F90=ifort
+
